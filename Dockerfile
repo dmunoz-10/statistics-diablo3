fc8c2edc4fd8e74f75b2b5038c1786c85414770f
Daniel Muñoz <djmv225@gmail.com>
2020-08-20 00:25:22 -0500

    Project setup

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:lts-alpine
+
+# set working directory
+WORKDIR /app
+
+# add `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+
+# install and cache app dependencies
+COPY package.json /app/package.json
+COPY yarn.lock /app/yarn.lock
+RUN yarn install
+RUN yarn global add @vue/cli
+
+# start app
+CMD ["yarn", "serve"]
---
+++ src/api/oauth.js
@@ -0,0 +1,23 @@
+import { post } from 'axios'
+
+const clientId = 'b3db715f4720438e9aedab0ecc9e5788'
+const clientSecret = 'MxMtoL2Q0fU7cx6NxpP6mRiLbssjPRhZ'
+
+const region = 'eu'
+const API_URL = `https://${region}.battle.net/oauth/token`
+
+function getToken() {
+  const body = new FormData()
+  body.append('grant_type', 'client_credentials')
+
+  const config = {
+    headers: { 'Content-Type': 'multipart/form-data' },
+    auth: { username: clientId, password: clientSecret },
+  }
+
+  return post(API_URL, body, config)
+}
+
+export {
+  getToken,
+}
